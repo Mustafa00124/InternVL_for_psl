dcbd91c79685f460bb9f0ccd761bd85ec0d4f0ee
SAJJAD GORAYA <135829301+SAJJADGORAYA1@users.noreply.github.com>
2025-07-27 01:22:31 +0100

    Update lora_tuning.sh

--- internvl_chat/shell/internvl2.5/2nd_finetune/lora_tuning.sh
+++ internvl_chat/shell/internvl2.5/2nd_finetune/lora_tuning.sh
@@ -6,7 +6,8 @@ BATCH_SIZE=${BATCH_SIZE:-4}
 PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-4}
 GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
 
-export PYTHONPATH=$(pwd)/internvl_chat
+# CORRECTED PYTHONPATH - matches your project structure
+export PYTHONPATH=$(pwd)/InternVL_for_psl/internvl_chat
 export INTERNVL_DISABLE_FLASH_ATTN=1
 export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
@@ -18,13 +19,16 @@ if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
+# CORRECTED TRAINING SCRIPT PATH - absolute path to your file
+TRAIN_SCRIPT="/content/InternVL_for_psl/internvl_chat/internvl/train/internvl_chat_finetune.py"
+
 torchrun \
   --nnodes=1 \
   --node_rank=0 \
   --master_addr=127.0.0.1 \
   --nproc_per_node=${GPUS} \
   --master_port=${MASTER_PORT} \
-  internvl_chat/internvl/train/internvl_chat_finetune.py \
+  "$TRAIN_SCRIPT" \  # Now using the correct path
   --model_name_or_path "OpenGVLab/InternVL2_5-1B" \
   --conv_style "internvl2_5" \
   --use_fast_tokenizer False \
